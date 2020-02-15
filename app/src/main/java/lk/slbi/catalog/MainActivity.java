7a7ee34e500139d1a5acfb33aef4c15b5b3af43e
Rikaz <iamrikas@gmail.com>
2020-02-15 18:32:57 +0530

    Added Internet Connection Check & Exit Alert

--- app/src/main/java/lk/slbi/catalog/MainActivity.java
+++ app/src/main/java/lk/slbi/catalog/MainActivity.java
@@ -2,7 +2,12 @@ package lk.slbi.catalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.CookieManager;
@@ -19,51 +24,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = findViewById(R.id.webView);
-        progressBar = findViewById(R.id.progressBar);
-        progressBar.setMax(100);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDisplayZoomControls(false);
+        String url = "http://catalog.slbi.lk";
 
-        // Dark Text on Status Bar
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-
-        webView.loadUrl("http://catalog.slbi.lk");
-        webView.setWebViewClient(new WebViewClient());
-        if (android.os.Build.VERSION.SDK_INT >= 21) {
-            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        if (!isConnected()) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Internet Connection Alert")
+                    .setMessage("Check Your Internet Connection and Try again!")
+                    .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
         } else {
-            CookieManager.getInstance().setAcceptCookie(true);
+            webView = findViewById(R.id.webView);
+            progressBar = findViewById(R.id.progressBar);
+            progressBar.setMax(100);
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setDisplayZoomControls(false);
+
+            // Dark Text on Status Bar
+            if (android.os.Build.VERSION.SDK_INT >= 23) {
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+
+            webView.loadUrl(url);
+            webView.setWebViewClient(new WebViewClient());
+            if (android.os.Build.VERSION.SDK_INT >= 21) {
+                CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+            } else {
+                CookieManager.getInstance().setAcceptCookie(true);
+            }
+
+            webView.setWebChromeClient(new WebChromeClient() {
+                @Override
+                public void onProgressChanged(WebView view, int progress) {
+                    super.onProgressChanged(view, progress);
+                    if (progress < 100 && progressBar.getVisibility() == ProgressBar.GONE) {
+                        progressBar.setVisibility(ProgressBar.VISIBLE);
+                    }
+
+                    progressBar.setProgress(progress);
+                    if (progress == 100) {
+                        progressBar.setVisibility(ProgressBar.GONE);
+                    }
+                }
+
+
+                @Override
+                public void onReceivedTitle(WebView view, String title) {
+                    super.onReceivedTitle(view, title);
+                }
+
+                @Override
+                public void onReceivedIcon(WebView view, Bitmap icon) {
+                    super.onReceivedIcon(view, icon);
+                }
+            });
         }
-
-        webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public void onProgressChanged(WebView view, int progress) {
-                super.onProgressChanged(view, progress);
-                if(progress < 100 && progressBar.getVisibility() == ProgressBar.GONE){
-                    progressBar.setVisibility(ProgressBar.VISIBLE);
-                }
-
-                progressBar.setProgress(progress);
-                if(progress == 100) {
-                    progressBar.setVisibility(ProgressBar.GONE);
-                }
-            }
-
-
-
-            @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-            }
-
-            @Override
-            public void onReceivedIcon(WebView view, Bitmap icon) {
-                super.onReceivedIcon(view, icon);
-            }
-        });
     }
 
     @Override
@@ -72,8 +93,30 @@ public class MainActivity extends AppCompatActivity {
             webView.goBack();
         } else {
 
-            super.onBackPressed();
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Exit")
+                    .setMessage("Do you want to exit?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    .show();
         }
+    }
+
+    private Boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 
 
