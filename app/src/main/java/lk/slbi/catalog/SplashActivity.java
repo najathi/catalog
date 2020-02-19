8ec7db9c7c7fe1cdc703fefc2f0d13bbddf0ad8c
Rikaz <iamrikas@gmail.com>
2020-02-19 23:10:03 +0530

    finish() Splash Activity after main_activity opened.

--- app/src/main/java/lk/slbi/catalog/SplashActivity.java
+++ app/src/main/java/lk/slbi/catalog/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -27,6 +28,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animation animation) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
 
             @Override
